0298569c834c0b3fce487394fd3865e592c03d9a
leoldding <leo.ld.ding@gmail.com>
2022-12-20 14:11:44 -0500

    created drag and drop zone

--- src/dragAndDrop.js
+++ src/dragAndDrop.js
@@ -1,12 +1,79 @@
 import React from 'react'
 import './styles.css'
+import './dragAndDrop.css'
 
 class DragAndDrop extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.dragCount = 0;
+        this.fileInput = React.createRef();
+        this.dropZone = React.createRef();
+    }
+
+    componentDidMount() {
+        this.dragCount = 0;
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        console.log("Handle Submit");
+    }
+
+    handleFileInput = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        console.log("Handle File Input");
+        this.fileInput.current.click();
+    }
+
+    handleDragEnter = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        this.dragCount++;
+        console.log("Handle Drag Enter");
+        if (!this.dropZone.current.classList.contains('dragBorder') && this.dragCount > 0) {
+            this.dropZone.current.classList.add('dragBorder');
+        }
+    }
+
+    handleDragLeave = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        this.dragCount--;
+        console.log("Handle Drag Leave");
+        if (this.dropZone.current.classList.contains('dragBorder') && this.dragCount <= 0) {
+            this.dropZone.current.classList.remove('dragBorder');
+        }
+    }
+
+    handleDragOver = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        console.log("Handle Drag Over");
+    }
+
+    handleDrop = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        this.dragCount = 0
+        console.log("Handle Drop");
+        this.dropZone.current.classList.remove('dragBorder');
+    }
+
     render() {
         return(
-            <div>
-                <p>Drag and Drop</p>
-            </div>
+            <form id={'fileUploadForm'} onSubmit={this.handleSubmit}>
+                <input type={'file'} ref={this.fileInput} id={'hiddenFileInput'}/>
+                <div ref={this.dropZone} className={'dragAndDrop'} onClick={this.handleFileInput}
+                     onDragEnter={this.handleDragEnter} onDragLeave={this.handleDragLeave} onDragOver={this.handleDragOver} onDrop={this.handleDrop}>
+                    <p>Drag and Drop Here</p>
+                    <p>or</p>
+                    <button id={'selectFile'}>Select File</button>
+                </div>
+                <button>Upload File</button>
+            </form>
         )
     }
 }
