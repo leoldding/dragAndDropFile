1b794848c14116af12b6eb7931a35b8479f55b6a
leoldding <leo.ld.ding@gmail.com>
2022-12-20 11:53:28 -0500

    initial commit

---
+++ src/dragAndDrop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles.css'
+
+class DragAndDrop extends React.Component {
+    render() {
+        return(
+            <div>
+                <p>Drag and Drop</p>
+            </div>
+        )
+    }
+}
+
+export default DragAndDrop;
